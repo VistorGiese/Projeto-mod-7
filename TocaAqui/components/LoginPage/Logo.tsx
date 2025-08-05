56bcd7b400f31ce2e73c238b3004e0ac1627b170
Vistorgiese <Vitorgiese@hotmail.com>
2025-08-04 21:46:38 -0300

    arquitetura back

---
+++ TocaAqui/components/LoginPage/Logo.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import MaskedView from "@react-native-masked-view/masked-view";
+import { useFonts } from "expo-font";
+import { customFonts } from "../../assets/fonts/fonts";
+
+export default function Logo() {
+  const [fontsLoaded] = useFonts(customFonts);
+
+  if (!fontsLoaded) return null;
+
+  return (
+    <View style={styles.container}>
+      <MaskedView
+        style={styles.maskedView}
+        maskElement={
+          <View style={styles.maskContainer}>
+            <Text style={styles.maskText}>Toca{"\n"}Aqui</Text>
+          </View>
+        }
+      >
+        <Image
+          source={require("../../assets/images/TelaInicial/BackgroundText.png")}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </MaskedView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  maskedView: {
+    height: 180,
+    width: 350,
+  },
+  maskContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+  maskText: {
+    fontSize: 80,
+    fontFamily: "AkiraExpanded-Superbold",
+    color: "black",
+  },
+  image: {
+    flex: 1,
+    width: 350,
+    height: 180,
+  },
+});
