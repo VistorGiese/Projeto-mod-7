00c05a4e1bee75b287b365c30177de4157532e39
alanakov <angelikovalski@gmail.com>
2025-08-28 21:51:02 -0300

    refactor(ArtistType): improve code formatting and consistency

--- TocaAqui/components/Allcomponents/ArtistType.tsx
+++ TocaAqui/components/Allcomponents/ArtistType.tsx
@@ -1,42 +1,40 @@
-import { Text, View, StyleSheet, } from "react-native";
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-
-
+import { Text, View, StyleSheet } from "react-native";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 interface ArtistTypeProps {
-    type: string;
+  type: string;
 }
 
 const typeName: Record<string, string> = {
-    solo: "microphone",
-    banda: "guitar",
-    dj: "compact-disc"
+  solo: "microphone",
+  banda: "guitar",
+  dj: "compact-disc",
 };
 
-
 function getTypeName(type: string) {
-    return typeName[type.toLowerCase()] || "user";
+  return typeName[type.toLowerCase()] || "user";
 }
 
 export default function ArtistType({ type }: ArtistTypeProps) {
-    return (
-        <View style={styles.container}>
-            <FontAwesome5 name={getTypeName(type) as any} size={18} color="#7381A8" />
-            <Text style={styles.text}>{type}</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <FontAwesome5 name={getTypeName(type) as any} size={18} color="#7381A8" />
+      <Text style={styles.text}>{type}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 8,
-    },
-    text: {
-        color: "#7381A8",
-        fontSize: 14,
-        fontWeight: "500",
-    }
-}) 
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+    marginBottom: 14,
+  },
+  text: {
+    color: "#7381A8",
+    fontSize: 14,
+    fontWeight: "500",
+  },
+});
