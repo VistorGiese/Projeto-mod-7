7e361b9b4a8b20029bf3317a6bce29ee78bbb62a
alanakov <angelikovalski@gmail.com>
2025-09-03 16:09:03 -0300

    refactor(CardArtist): enhance layout and styling for improved UI consistency

--- TocaAqui/components/Allcomponents/CardArtist.tsx
+++ TocaAqui/components/Allcomponents/CardArtist.tsx
@@ -6,7 +6,11 @@ import Stars from "./Stars";
 export default function CardArtist() {
   return (
     <View style={styles.container}>
-      <Image source={require("../../assets/images/TelaInicial/artist.png")} />
+      <Image
+        source={require("../../assets/images/TelaInicial/artist.png")}
+        width={100}
+        height={100}
+      />
       <View style={styles.containerContent}>
         <Text style={styles.containerTextName}>Nome do cantor</Text>
         <View style={styles.containerTag}>
@@ -16,7 +20,9 @@ export default function CardArtist() {
           <ArtistType type="Solo" />
           <Stars starNumber={4} />
         </View>
-        <TouchableOpacity>Ver perfil</TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Ver perfil</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -26,17 +32,18 @@ const styles = StyleSheet.create({
   container: {
     color: "#fff",
     backgroundColor: "rgba(255, 255, 255, 0.1)",
-    gap: 8,
     borderRadius: 10,
   },
   containerContent: {
-    paddingHorizontal: 20,
+    paddingHorizontal: "5%",
+    gap: 8,
   },
   containerTextName: {
     color: "#fff",
     fontSize: 16,
     fontWeight: "semibold",
-    paddingTop: 14,
+    paddingTop: "6%",
+    paddingVertical: "2%",
   },
   containerTag: {
     flexDirection: "row",
@@ -46,6 +53,20 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    marginBottom: 14,
+    marginBottom: "2%",
+    paddingVertical: "2%",
+  },
+  button: {
+    paddingVertical: "5%",
+    borderWidth: 1.5,
+    borderRadius: 12,
+    borderColor: "#7381A8",
+    color: "#7381A8",
+  },
+  buttonText: {
+    color: "#7381A8",
+    textAlign: "center",
+    fontFamily: "Montserrat",
+    fontWeight: "semibold",
   },
 });
